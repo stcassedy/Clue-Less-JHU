6945b2baff9f4ca7da4dc070e55358d43af1af58
Lance Gardner <lancegardner10@gmail.com>
2018-04-25 21:25:06 -0400

    Changes to make the UI listen to the buttons being pushed

--- Client/src/UI/ClientWindow.cpp
+++ Client/src/UI/ClientWindow.cpp
@@ -12,3 +12,33 @@ ClientWindow::~ClientWindow()
 {
     delete ui;
 }
+
+void ClientWindow::on_NavigateBoard_clicked()
+{
+    ui->GameActions->setText("You clicked\nNavigateBoard");
+}
+
+void ClientWindow::on_MakeSuggestion_clicked()
+{
+    ui->GameActions->setText("You clicked\nMake Suggestion");
+}
+
+void ClientWindow::on_Refute_clicked()
+{
+    ui->GameActions->setText("You clicked\nRefute");
+}
+
+void ClientWindow::on_MakeAccusation_clicked()
+{
+    ui->GameActions->setText("You clicked\nMake Accusation");
+}
+
+void ClientWindow::on_ViewNotebook_clicked()
+{
+    ui->GameActions->setText("You clicked\nView Notebook");
+}
+
+void ClientWindow::on_EndTurn_clicked()
+{
+    ui->GameActions->setText("You clicked\nEnd Turn");
+}

fe6bd1ba09268f0db4452c71f8ebb3f339d9b71a
Lance Gardner <lancegardner10@gmail.com>
2018-04-27 19:37:04 -0400

    New header file, I forgot to include this on a previous commit

--- Client/src/UI/ClientWindow.h
+++ Client/src/UI/ClientWindow.h
@@ -15,6 +15,19 @@ public:
     explicit ClientWindow(QWidget *parent = 0);
     ~ClientWindow();
 
+private slots:
+    void on_NavigateBoard_clicked();
+
+    void on_MakeSuggestion_clicked();
+
+    void on_Refute_clicked();
+
+    void on_MakeAccusation_clicked();
+
+    void on_ViewNotebook_clicked();
+
+    void on_EndTurn_clicked();
+
 private:
     Ui::ClientWindow *ui;
 };

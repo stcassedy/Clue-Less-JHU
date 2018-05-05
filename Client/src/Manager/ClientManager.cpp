45c5e68afa947114edc194c0dc87bf58ed574449
rhahn1 <38504609+rhahn1@users.noreply.github.com>
2018-05-05 12:52:05 -0400

    Add files via upload

--- Client/src/Manager/ClientManager.cpp
+++ Client/src/Manager/ClientManager.cpp
@@ -1,6 +1,37 @@
 #include "ClientManager.h"
 
+#include "BoardEnums.h"
+#include "BoardElement.h"
+#include "Envelope.h"
+#include "Card.h"
+
+
+//---------------------------------------------------------------
+//Constructor:
 ClientManager::ClientManager()
+{
+
+}
+
+//---------------------------------------------------------------
+//Constructor:
+ClientManager::~ClientManager()
+{
+
+}
+
+//---------------------------------------------------------------
+//Public Functions:
+void ClientManager::movePlayer(BoardElement destination, BoardElement location)
+{
+    if(destination.isSpaceAvailable() == true && location.isBoardElementConnected(destination.getBoardElementEnum()) == true)
+    {
+        destination.addPlayer(currentPlayer);
+        location.removePlayer(currentPlayer);
+    }
+}
+
+void makeSuggestion()
 {
 
 }
